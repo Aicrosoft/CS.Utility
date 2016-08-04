56836d9e32485c49ab4cf49717a76b9b5c83a331
atwind <atwind@cszi.com>
2016-08-04 15:32:39 +0800

    增加Log4NetAdapter的Demo项目，发现依赖的log4net版本不对，修正Log4NetAdapter中的log4net版本依赖

---
+++ sources/CS.Utility.Demos/CS.Utility.Log4NetAdapterDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CS.Logging;
+
+namespace CS.Utility.Log4NetAdapterDemo
+{
+    class Program
+    {
+        static Program() 
+        {
+            log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo(Consts.Log4NetConfigFile));
+            //注意，还要有AssemblyInfoLog4Net.cs里的相关代码
+        }
+
+        static void Main(string[] args)
+        {
+            ILog _sysLog = LogManager.GetLogger(typeof(Program));
+            ILog _appLog = LogManager.GetLogger();
+
+
+            _sysLog.Debug($"{DateTime.Now}");
+
+            _appLog.Debug($"App日志输出，{DateTime.Now}");
+
+        }
+    }
+}
