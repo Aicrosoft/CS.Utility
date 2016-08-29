4088d617c3e86e48b35e023489ff9116aa7e5799
atwind <atwind@cszi.com>
2016-08-29 14:46:01 +0800

    增加XmlSerialize的扩展，注意，Xml序列化不一定能将目标对象序列化成功。

---
+++ sources/CS.Utility/Extension/XmlSerializeExt.cs
@@ -0,0 +1,17 @@
+﻿using CS.Serialization;
+
+namespace CS.Extension
+{
+    public static class XmlSerializeExt
+    {
+        public static string ToXml(this object o)
+        {
+            return XmlSerializor.Serialize(o);
+        }
+
+        public static T FromXml<T>(this string xml)
+        {
+            return XmlSerializor.Deserialize<T>(xml);
+        }
+    }
+}
