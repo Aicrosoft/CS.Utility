db495488923d754c0e9c7f395111061647870b22
atwind <atwind@cszi.com>
2016-09-08 11:22:43 +0800

    降低对.NetFramework的依赖 增加CS.Utility.Extension (一般的基础Nuget依赖实现放这儿) 增加CS.Utility.Form Form相关的扩展和通用实现

---
+++ sources/CS.Utility.Form/Extension/FromExt.cs
@@ -0,0 +1,23 @@
+﻿namespace System.Windows.Forms
+{
+    /// <summary>
+    /// 尽量不用继承类来实现一些功能
+    /// </summary>
+    public static class FromExt
+    {
+
+        /// <summary>
+        /// 线程操作UI上的控件的值变更
+        /// </summary>
+        /// <param name="form"></param>
+        /// <param name="value"></param>
+        /// <param name="func"></param>
+        public static void ChangeValue(this Form form,string value, Action<string> func)
+        {
+            if (form.InvokeRequired)
+                form.Invoke(func, value);
+            else
+                func(value);
+        }
+    }
+}
--- sources/CS.Utility/Properties/AssemblyInfo.cs
+++ sources/CS.Utility/Properties/AssemblyInfo.cs
@@ -29,18 +29,20 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.3.1")]  //已取消CI脚本的版本号替换功能，该版本号将发布至NuGet上。Note:版本号不变化时将不会发布至Nuget上
+[assembly: AssemblyVersion("0.1.4.0")]  //已取消CI脚本的版本号替换功能，该版本号将发布至NuGet上。Note:版本号不变化时将不会发布至Nuget上
 //[assembly: AssemblyFileVersion("1.0.0.0")]
 
 
 /*
 
+v 0.1.4.0
+-----------------
+. 所有.NetFramework版本改为4.5
 
 v 0.1.3.0
 ------------------
 . 加入老赵的CodeTime效能计算类
 . 加入DebugConsole彩色控制台输出类
-
 
 v 0.1.0.2
 ------------------
